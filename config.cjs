5ef49c15eb862d2c5ecb2b27e147ad6179a7897a
Dame2007-beep <fall.dame201107@gmail.com>
2025-05-03 10:50:11 +0200

    Update config.cjs_meee

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "nector~lxZTXDCR#lZ_z55ylRk81YIvgUwF4S3Z56aLmT-FE_vPZDpeKFUc",
+  SESSION_ID: process.env.SESSION_ID || "xZTXDCR#lZ_z55ylRk81YIvgUwF4S3Z56aLmT-FE_vPZDpeKFUc",
   PREFIX: process.env.PREFIX || '*',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
